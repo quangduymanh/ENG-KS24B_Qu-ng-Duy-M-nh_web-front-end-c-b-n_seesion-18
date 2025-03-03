f2f31bf39f85c60d1a2f79855cbd20ae72358f73
quangduymanh <quangduymanh4@gmail.com>
2025-03-03 08:46:51 +0700

    upload bài 04

---
+++ seesion 18-bài 04.js
@@ -0,0 +1,35 @@
+let number = +prompt ("nhap vào một số từ 0-9");
+switch (number) {
+    case 0:
+        document.write("Số Không");
+        break;
+    case 1:
+        document.write("Số Một");
+        break;
+    case 2:
+        document.write("Số Hai");
+        break;
+    case 3:
+        document.write("Số Ba");
+        break;
+    case 4:
+        document.write("Số Bốn");
+        break;
+    case 5:
+        document.write("Số Năm");
+        break;
+    case 6:
+        document.write("Số Sáu");
+        break;
+    case 7:
+        document.write("Số Bảy");
+        break;
+    case 8:
+        document.write("Số Tám");
+        break;
+    case 9:
+        document.write("Số Chín");
+        break;
+    default:
+        document.write("Vui lòng nhập số từ 0 đến 9");
+}
